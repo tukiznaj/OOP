3782727e6489e9ce93a9e6d7a5b6daf9e26241b9
Mohamed Najulah <Najulah@Admins-MacBook-Pro.local>
2018-01-12 14:39:45 +0800

    moved example files to examples folder

---
+++ examples/MethodDemo.java
@@ -0,0 +1,16 @@
+public class MethodDemo{
+    // static method
+    public static void brainWash(){
+        System.out.println("i love java");
+    }
+
+    // static method
+    public static void seriousBrainWash(){
+        System.out.println("Serious brain wash");
+    }
+
+    public static void main(String[] args){
+        brainWash();
+        seriousBrainWash();
+    }
+}
